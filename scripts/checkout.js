312ca0cbab8e44cef4de036484632ebc1f05fbf0
Jirotgak Dyk Gotau <dykjirotgak@gmail.com>
2025-09-22 20:40:31 +0100

    save data, generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,7 @@
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOrderSummary();
+
+renderPaymentSummary();
+
