f23ba8d16b5602b417a651c6da1425ef8d68a1ee
Jirotgak Dyk Gotau <dykjirotgak@gmail.com>
2025-10-06 17:16:28 +0100

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,6 +1,47 @@
-export function getProduct(productId) {
+/*export function getProduct(productId) {
   return products.find(product => product.id === productId);
 }
+  */
+import { formatCurrency } from "../scripts/utils/money.js";
+
+export function getProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+}
+
 
 export const products = [
   {
@@ -661,4 +702,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
