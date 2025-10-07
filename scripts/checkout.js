e3063d6ba6b498b74f1b7e5624b8eabd932fec06
Jirotgak Dyk Gotau <dykjirotgak@gmail.com>
2025-10-07 20:47:43 +0100

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,8 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //for an import that runs the code inside a file like an oop file, we use an import syntax that runs all the code without importing anything like this line below:
 //import '../data/cart-class.js'
 
+import '../data/backend-practice.js'
+
 renderOrderSummary();
 
 renderPaymentSummary();
