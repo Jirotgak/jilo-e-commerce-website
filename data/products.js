565de60df3895d3a81ab5a1e03b6d063b75d6534
Jirotgak Dyk Gotau <dykjirotgak@gmail.com>
2025-10-07 13:08:59 +0100

    learn more details about classes and the use of "this"

--- data/products.js
+++ data/products.js
@@ -62,7 +62,37 @@ class Clothing extends Product {
     </a>
     `;
   }
+}
+
+/*
+const date = new Date();
+console.log(date);
+const time = date.toLocaleTimeString();
+console.log(time);
+*/
+/*
+const object2 = {
+  a: 2,
+  b: this.a
 };
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+object3.method();
+
+*/
 
 
 export const products = [
